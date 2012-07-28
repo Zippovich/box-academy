e61a89a970b75c1b95cf4097b17fa1cb455abc27
Антон Цепковский <zippovich@gmail.com>
2012-07-28 04:29:57 +0300

    Changes in header, content, add gallery, page image, crossbrowser compatibility

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    $("#tS2").thumbnailScroller({
+        scrollerType: "clickButtons",
+        scrollerOrientation: "horizontal",
+        scrollSpeed: 2,
+        scrollEasing: "easeOutCirc",
+        scrollEasingAmount: 600,
+        acceleration: 4,
+        scrollSpeed: 800,
+        noScrollCenterSpace: 10,
+        autoScrolling: 0,
+        autoScrollingSpeed: 2000,
+        autoScrollingEasing: "easeInOutQuad",
+        autoScrollingDelay: 500
+    });
+});
